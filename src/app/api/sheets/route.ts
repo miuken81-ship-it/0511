19d0b8e8d40400c9bf7c4d77396d81a28eca7fbe
miuken81-ship-it <miuken81@gmail.com>
2025-12-24 22:52:30 +0700

    Update route.ts

--- src/app/api/sheets/route.ts
+++ src/app/api/sheets/route.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { NextRequest, NextResponse } from 'next/server';
 
-const SHEET_ID = process.env.SHEET_ID || '1sPgW_Rqr8utPR48qExEZiODLW8xfyMUEfWg45t2Jz3Y';
-const SHEET_URL = 'https://script.google.com/macros/s/AKfycby-QlhA0WzYMyvT6-u9jVxdTdPiBqmszOJCXR807cjaQ65Pikau8wQzIfXiMkcwmK_7/exec';
+const SHEET_ID = process.env.SHEET_ID || '1OknzjjSG-uvCQ4_C09KzMt9eDX273NU3I9vwprj-LrY';
+const SHEET_URL = 'https://script.google.com/macros/s/AKfycbxF6Z3vx4gw9dFHtW3XmKAMeFl4jQtToDrhKqYLNjEpaHa5DHwvSp8-U_cUpjA1vW0/exec';
 
 export async function POST(req: NextRequest) {
     try {
